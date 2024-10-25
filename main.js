c2993cd8cef42d73b674ca90805c59e355a64b9d
Luis Alberto Ibarra Calderon <ibarra.luisalberto23@gmail.com>
2024-10-25 11:52:48 -0400

    commit

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ let users = [
     new User(1, 'John Doe', 'johndoe', 'john@example.com', 'password123', new Date(), 'john.jpg', 'admin'),
     new User(2, 'Jane Smith', 'janesmith', 'jane@example.com', 'password123', new Date(), 'jane.jpg', 'user'),
     new User(3, 'Robert Brown', 'robbrown', 'robert@example.com', 'password123', new Date(), 'robert.jpg', 'user'),
-    new User(4, 'Luis Alberto', 'Ibarra', 'Ibarra.luisalberto23@gmail.com', '23luis', new Date(), 'perfil.jpg', 'user')
+    new User(4, 'Luis Alberto', 'Ibarra Calderon', 'Ibarra.luisalberto23@gmail.com', '23luis', new Date(), 'perfil.jpg', 'admin')
 ];
 
 // CRUD Endpoints
